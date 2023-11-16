2c7a97a2a770d024a5073d0b714ee35eec3e91c6
paras <parassapariya212@gmail.com>
2023-11-16 11:07:58 +0530

    make folder and env

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-hello
+const express = require('express');
+const app = express();
+const bodyparser = require('body-parser');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT,()=>{
+    console.log("server is running 5000");
+})
